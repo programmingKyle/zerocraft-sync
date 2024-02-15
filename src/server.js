65043e8d63985dc189d1b566d91af09758802d42
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-02-15 18:11:30 -0500

    App Host Functionality
    - If server is offline a server can start the server and they will be marked as host
    - Any users using the the app will not be able to see start or stop buttons.
    - This will limit users accidentally changing the gist data when it shouldn't be
    - Only the host can see the stop button and change the gist data

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const startServerButton_el = document.getElementById('startServerButton');
 const stopServerButton_el = document.getElementById('stopServerButton');
 
 let serverOnline = false;
+let isHost = false;
 
 function isServerOnline(){
     if (gist.status === 'ONLINE'){
         serverOnline = true;
         startServerButton_el.style.display = 'none';
+        if (!isHost){
+            stopServerButton_el.style.display = 'none';
+        }
     } else {
         serverOnline = false;
         stopServerButton_el.style.display = 'none';
@@ -15,10 +19,15 @@ function isServerOnline(){
 
 function toggleServerStatus(){
     if (!serverOnline){
+        isHost = true;
         serverOnline = true;
         startServerButton_el.style.display = 'none';
         stopServerButton_el.style.display = 'block';
+        
     } else {
+        if (isHost){
+            isHost = false;
+        }
         serverOnline = false;
         startServerButton_el.style.display = 'block';
         stopServerButton_el.style.display = 'none';
@@ -35,7 +44,7 @@ startServerButton_el.addEventListener('click', async () => {
 });
 
 stopServerButton_el.addEventListener('click', async () => {
-    if (serverOnline){
+    if (serverOnline && isHost){
         await getGist();
         if (gist.status === 'ONLINE'){
             console.log('Come on');
