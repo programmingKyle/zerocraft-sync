521b454227cc892d4bf675455ed92fafc1264297
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-02-15 17:21:17 -0500

    Information Imrpovements
    - If server is going to an offline state all server information will be null.
    - If server information is null it will be displayed as N/A.
    - This is basically a way to clear the server information right now so users don't see server offline but still see connect information

--- src/gist.js
+++ src/gist.js
@@ -17,17 +17,23 @@ async function populateInfo(data){
     } else if (serverStatusText_el.classList.contains('online')){
         serverStatusText_el.classList.remove('online');
     }
-    serverNameText_el.textContent = data.servername;
-    serverIPText_el.textContent = data.ip;
-    serverPortText_el.textContent = data.port;
+
+    const dataValueHandler = (entry) => {
+        const output = entry === null ? 'N/A' : entry;
+        return output;
+    }
+    
+    serverNameText_el.textContent = dataValueHandler(data.servername);
+    serverIPText_el.textContent = dataValueHandler(data.ip);
+    serverPortText_el.textContent = dataValueHandler(data.port);    
 }
 
 async function updateGist(){
     const updatedContent = {
         "status": gist.status === 'OFFLINE' ? 'ONLINE' : 'OFFLINE',
-        "servername": settings.serverName,
-        "ip": settings.ip,
-        "port": '3306'
+        "servername": gist.status === 'OFFLINE' ? settings.serverName : null,
+        "ip": gist.status === 'OFFLINE' ? settings.ip : null,
+        "port": gist.status === 'OFFLINE' ? '3306' : null
     }
     const updateSuccess = await api.gistHandler({request: 'Update', gistID: settings.gistID, accessToken: settings.accessToken, updatedContent});
     if (updateSuccess){
